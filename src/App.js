0196a0b300929462c86727b770b4ac13baac97e9
RoliVegas <roland.krancz@gmail.com>
2022-11-21 15:11:49 +0100

    Removed title

--- src/App.js
+++ src/App.js
@@ -7,12 +7,9 @@ import './App.css'
 export default function App() {
     return (
         <div className='App'>
-        <div className='title'>
-            <h1>MARS by Roland Krancz</h1>
-        </div>
         <Canvas className='canvas'>
             <ambientLight intensity={0.1} />
-            <pointLight color='#rgb(244, 243, 233)' position={[2,0,10]} intensity={0.8} />
+            <pointLight color='rgb(244, 243, 233)' position={[2,0,10]} intensity={0.8} />
             <Suspense fallback={null}>
                 <Stars />
                 <Mars />
